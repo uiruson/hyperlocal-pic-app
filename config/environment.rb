556615867b8a815d286c925abbbc1228b0e209f7
vagrant <vagrant@lighthouse-labs>
2014-11-22 02:41:18 +0000

    login

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,7 @@ require 'active_support/all'
 # Load Sinatra Framework (with AR)
 require 'sinatra'
 require 'sinatra/activerecord'
-require 'omniauth-instagram'
+
 require 'pry'
 
 APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
@@ -23,21 +23,21 @@ configure do
   secrets = YAML.load_file( File.join(Sinatra::Application.root, 'config', 'secrets.yml') )
   set :instagram_id, secrets['instagram']['id']
   set :instagram_secret, secrets['instagram']['secret']
-
+  
   set :views, File.join(Sinatra::Application.root, "app", "views")
 end
 
 # Set up the database and models
 require APP_ROOT.join('config', 'database')
 
-db = URI.parse('postgres://postgres:postgres@localhost:5432/hyperlocal')
+# db = URI.parse('postgres://vagrant@localhost:5432/hyperlocal')
 
 ActiveRecord::Base.establish_connection(
-  :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-  :host     => db.host,
-  :username => db.user,
-  :password => db.password,
-  :database => db.path[1..-1],
+  :adapter  => 'postgresql',#? 'postgresql' : db.scheme,
+  :host     => 'localhost',
+  :username => 'hyperlocal',
+  :password => 'hyperlocal',
+  :database => 'hyperlocal',
   :encoding => 'utf8',
   :port => 5432
 )
