cc6d78e6f9c4c394b1839b971f12df8f8fc3b2ca
sarahca <s.canadabc@gmail.com>
2014-11-22 19:22:21 -0800

    adding upload panel

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,39 +1,62 @@
-
-
-// See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
-
 $(document).ready(function(){
 
   // second page should be invisible at the beginning
-  $('#second-page').css({display : "none"});
+  // $('#second-page').css({display : "none"});
 
   // login/register panel invisible at the beginning
   $('#right-panel-register').css({display : "none"});
 
   // clicking on the arrow makes the side panel visible
+  // $('#right-panel-link').on('click', function(){
+  //   console.log("clicked the arrow");
+  // });
   $('#right-panel-link').panelslider({side: 'right', clickClose: false, duration: 200 });
 
   //clicking on register in the side panel shows the registration form
   // and makes the login form disappear
   $('#register').on('click', function(){
-    $(this).css({display : "none"});
+    $('#register-button').hide();
     $('#right-panel-login').css({display : "none"});
     $('#right-panel-register').css({display : "block"});
   })
 
 
-  // SECOND PAGE
-  //clicking on the loggin button makes the second page visible
+  // // SECOND PAGE
+  // //clicking on the loggin button makes the second page visible
   $('#login-button').on('click', function(){
-    console.log("clicked login")
-    $('#first-page').css({display : "none"});
-    $('#second-page').css({display : "block"});
+    console.log("clicked login");
+    $('#first-page').hide();
+    $('#second-page').show();
   });
 
-  //sliding panel to upload pictures
-  $('.pull-me').click(function() {
-        $('.upload-panel').slideToggle('slow');
-    });
+  $('#upload-panel-button').on('click', function(){
+     var uploadPanel = $('.upload-panel');
+     if (uploadPanel.is(':hidden')) {
+        uploadPanel.slideDown(300);
+        $(this).css({display : "none"})
+     }
+  });
+
+  //clicking on "upload" to upload the picture closes the panel
+  //the upload and sign out buttons become visible again
+  $('#upload-picture').on('click', function(){
+    console.log("clicked upload button")
+    var uploadPanel = $('.upload-panel');
+    uploadPanel.slideUp(300);
+    $('#upload-panel-button').css({display : "block"});
+  });
+
+  //same behavior as clicking on the "upload button"
+  $('#cancel-upload').on('click', function(){
+    console.log("clicked upload button")
+    var uploadPanel = $('.upload-panel');
+    uploadPanel.slideUp(300);
+    $('#upload-panel-button').css({display : "block"});
+  });
+
+  // $('.placeholder').on('click', function(){
+  //   console.log("clicked on picture");
+  // });
 });
 
 
