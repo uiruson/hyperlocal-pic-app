2cdbda0fa47e910b90d3fcd6ece48c6f98fa3fd8
vagrant <vagrant@lighthouse-labs>
2014-12-02 02:37:21 +0000

    removed pry in environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,7 @@ require 'active_support/all'
 # Load Sinatra Framework (with AR)
 require 'sinatra'
 require 'sinatra/activerecord'
-require 'pry' if development?
+
 require 'gon-sinatra'
 
 APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
